0b7514e71abd5d03698dc71b964ec33d589eaff4
mohammedOtaq <mohammed.otaq@gmail.com>
2025-04-09 19:19:05 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from openai import OpenAI
+client = OpenAI()
+
+my_assistant = client.beta.assistants.create(
+    instructions="You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
+    name="Math Tutor",
+    tools=[{"type": "code_interpreter"}],
+    model="gpt-4o",
+)
+print(my_assistant)
 
 import streamlit as st
 import json
